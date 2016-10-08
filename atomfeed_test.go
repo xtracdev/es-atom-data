10b6773b06b1e2b26b6dce75dfe71ad863cf7e75
d-smith <doug.smith.mail@gmail.com>
2016-10-08 06:18:15 -0700

    Added unit test for getRecentFeedCount

--- atomfeed_test.go
+++ atomfeed_test.go
@@ -3,10 +3,10 @@ package esatompub
 import (
 	"errors"
 	"github.com/stretchr/testify/assert"
+	"github.com/xtracdev/goes"
 	"gopkg.in/DATA-DOG/go-sqlmock.v1"
 	"os"
 	"testing"
-	"github.com/xtracdev/goes"
 )
 
 func TestSetThresholdFromEnv(t *testing.T) {
@@ -20,10 +20,10 @@ func TestSetThresholdToDefaultOnBadEnvSpec(t *testing.T) {
 	os.Setenv("FEED_THRESHOLD", "two")
 	ReadFeedThresholdFromEnv()
 	assert.Equal(t, defaultFeedThreshold, FeedThreshold)
+	os.Setenv("FEED_THRESHOLD", "2")
 }
 
 func TestReadPreviousFeedId(t *testing.T) {
-	os.Unsetenv("FEED_THRESHOLD")
 	db, mock, err := sqlmock.New()
 	if err != nil {
 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
@@ -89,7 +89,6 @@ func TestReadPreviousFeedIdScanError(t *testing.T) {
 	}
 }
 
-
 func TestWriteEvent(t *testing.T) {
 	db, mock, err := sqlmock.New()
 	if err != nil {
@@ -108,8 +107,27 @@ func TestWriteEvent(t *testing.T) {
 	mock.ExpectExec("insert into atom_event")
 
 	tx, _ := db.Begin()
-	err = writeEventToAtomEventTable(tx,eventPtr)
-	assert.Nil(t,nil)
+	err = writeEventToAtomEventTable(tx, eventPtr)
+	assert.Nil(t, nil)
 	err = mock.ExpectationsWereMet()
 	assert.Nil(t, err)
+}
+
+func TestGetRecentFeedCount(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	rows := sqlmock.NewRows([]string{"count(*)"}).AddRow(23)
+
+	mock.ExpectBegin()
+	mock.ExpectQuery(`select count\(\*\) from atom_event where feedid is null`).WillReturnRows(rows)
+	tx, _ := db.Begin()
+	count, err := getRecentFeedCount(tx)
+	if assert.Nil(t, err) {
+		assert.Equal(t, count, 23)
+	}
+
 }
