b4a99010d2eb9daab0480315faa8dd1af7a230f6
d-smith <doug.smith.mail@gmail.com>
2016-10-12 14:48:21 -0700

    unit test covered rows.Err

--- atomdataquery_test.go
+++ atomdataquery_test.go
@@ -55,4 +55,47 @@ func TestQueryForRecentQueryError(t *testing.T) {
 	}
 }
 
+func TestQueryForRecentScanError(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
 
+	foo := struct {
+		foo string
+		bar string
+	}{
+		"foo", "bar",
+	}
+	rows := sqlmock.NewRows([]string{"feedid"}).AddRow(foo)
+	mock.ExpectQuery("select").WillReturnRows(rows)
+
+	_, err = RetrieveRecent(db)
+	if assert.NotNil(t,err) {
+		err = mock.ExpectationsWereMet()
+		assert.Nil(t, err, "expectations not met in TestQueryForRecentQueryError")
+	}
+}
+
+func TestQueryForRecentFinalRowsError(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	ts := time.Now()
+	rows := sqlmock.NewRows([]string{"event_time", "aggregate_id",
+		"version", "typecode", "payload"},
+	).AddRow(ts, "1x2x333", 3, "foo", []byte("yeah ok")).RowError(0,errors.New("dang"))
+	mock.ExpectQuery("select").WillReturnRows(rows)
+
+	_, err = RetrieveRecent(db)
+	if assert.NotNil(t,err) {
+		assert.Equal(t, "dang", err.Error())
+		err = mock.ExpectationsWereMet()
+		assert.Nil(t, err, "expectations not met in TestQueryForRecentQueryError")
+	}
+
+}
